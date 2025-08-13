8cd2c4cea9fbdc096a165ad8dc1bfa4819f083e8
suvaan2017 <suvaan101@gmail.com>
2025-08-13 17:02:34 +0545

    suvaan

---
+++ activity 1.py
@@ -0,0 +1,10 @@
+tree1 = 98
+tree2 = 45
+tree3 = 67
+tree4 = 23
+tree5 = 89
+
+sum = tree1 + tree2 + tree3 + tree4 + tree5
+print("The total number of trees is:", sum)
+average = sum / 5
+print("The average number of trees is:", average)
